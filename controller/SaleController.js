a07f91335fd7ddc53478cc0c28c7a4cb68848aab
teerakarn-note <340562teerakarn@gmail.com>
2025-10-21 10:36:25 +0700

    add

--- controller/SaleController.js
+++ controller/SaleController.js
@@ -1,6 +1,7 @@
 const express = require('express');
 const app = express.Router();
 const { PrismaClient } = require('@prisma/client');
+const { error } = require('console');
 const prisma = new PrismaClient();
 
 app.post('/save', async (req, res) => {
@@ -45,7 +46,7 @@ app.get('/list', async (req, res) => {
             orderBy: {
                 id: 'desc'
             }
-            
+
         })
         res.send({ results: results });
     } catch (e) {
@@ -57,7 +58,7 @@ app.get('/billInfo/:billSaleId', async (req, res) => {
     try {
         const result = await prisma.billSaleDetail.findMany({
             //เชื่อมตาราง
-            include:{
+            include: {
                 Product: true
             },
             where: {
@@ -72,7 +73,93 @@ app.get('/billInfo/:billSaleId', async (req, res) => {
     } catch (e) {
         res.status(500).send({ error: e.message });
     }
+})
+app.get('/updateStatusToPay/:billSaleId', async (req, res) => {
+    try {
+        await prisma.billSale.update({
+            data: {
+                status: 'pay'
+            },
+            where: {
+                id: parseInt(req.params.billSaleId)
+            }
+        })
+        res.send({ message: 'success' })
+    }
+    catch (e) {
+        res.status(500).send({ error: e.message })
+    }
+})
 
+app.get('/updateStatusToSend/:billSaleId', async (req, res) => {
+    try {
+        await prisma.billSale.update({
+            data: {
+                status: 'send'
+            },
+            where: {
+                id: parseInt(req.params.billSaleId)
+            }
+        })
+        res.send({ message: 'success' })
+    }
+    catch (e) {
+        res.status(500).send({ error: e.message })
+    }
+})
+app.get('/updateStatusToCancel/:billSaleId', async (req, res) => {
+    try {
+        await prisma.billSale.update({
+            data: {
+                status: 'cancel'
+            },
+            where: {
+                id: parseInt(req.params.billSaleId)
+            }
+        })
+        res.send({ message: 'success' })
+    }
+    catch (e) {
+        res.status(500).send({ error: e.message })
+    }
+})
+app.get('/dashboard', async (req, res) => {
+    try {
+        let arr = [];
+        let myDate = new Date();
+        let year = myDate.getFullYear();
+        for (let i = 1; i <= 12; i++) {
+            const daysInMonth = new Date(year, i, 0).getDate();
+            console.log(daysInMonth);
+            const billSaleInMonth = await prisma.billSale.findMany({
+                where: {
+                    payDate: {
+                        gte: new Date(year + '-' + i + '-01'),
+                        lte: new Date(year + '-' + i + '-' + daysInMonth)
+                    }
+                }
+            })
+            let sumPrice = 0;
+            for (let j = 0; j < billSaleInMonth.length; j++) {
+                const billSaleObject = billSaleInMonth[j];
+                const sum = await prisma.billSaleDetail.aggregate({
+                    _sum: {
+                        price: true
+                    },
+                    where: {
+                        billSale_Id: billSaleObject.id
+                    }
+
+                })
+                sumPrice = sum._sum.price ?? 0;
+            }
+            arr.push({ month: i, sumPrice: sumPrice })
+        }
+        res.send({ results: arr });
+    }
+    catch (e) {
+        res.status(500).send({ error: e.message });
+    }
 })
 
 module.exports = app;
